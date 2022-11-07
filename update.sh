cd49d6dbc1e02839afb7f4c6b78e3bbc9300d370
dean <dean@run-withit.com>
2022-11-07 14:32:43 -0700

    trouble->bubble

--- update.sh
+++ update.sh
@@ -8,7 +8,7 @@ if [ "${1}" == "local" ] && [ -f /tmp/${COMPONENT}.${HOST_SYSTEM}.tar.gz ] ; the
     cp /tmp/${COMPONENT}.${HOST_SYSTEM}.tar.gz .
 else
     rm -f ${COMPONENT}.${HOST_SYSTEM}.tar.gz
-    wget http://trouble.bottorrent.net/static/RWI/runtime/${COMPONENT}.${HOST_SYSTEM}.tar.gz
+    wget http://bubble.bottorrent.net/static/RWI/runtime/${COMPONENT}.${HOST_SYSTEM}.tar.gz
 fi
 
 tar xvzf ${COMPONENT}.${HOST_SYSTEM}.tar.gz
